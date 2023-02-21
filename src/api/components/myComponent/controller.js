ef613dc07dc56511f12ba3bcabc136e4c0677087
Nelson Rivas <nelson.rivas.ucv+code@gmail.com>
2023-02-21 14:21:10 -0400

    Updating controllers for albums

--- src/api/components/myComponent/controller.js
+++ src/api/components/myComponent/controller.js
@@ -1,29 +1,21 @@
+// import { CustomError } from '../../../helpers/errorsHandler.js'
 import Album from './albums.js'
 
 export const testEndpoint = (req, resp) => {
   resp.send('Test endpoint!')
 }
 
-export const createAlbum = async (req, resp) => {
+export const createAlbum = async (req, resp, next) => {
   console.log(req.body)
   try {
     const album = await Album.create(req.body)
-    console.log('created:', album)
-    resp.send(album)
+    // resp.send(album)
+    resp.status(200).json({ status: 'success', data: album })
   } catch (error) {
     console.error(`Error creating album: ${error}`)
+    // throw new CustomError('Error creating album', 500)
+    // next(error)
     resp.status(500).send('Error creating album')
-  }
-}
-
-export const listAlbums = async (req, resp) => {
-  console.log(req.body)
-  try {
-    const albums = await Album.listAll()
-    resp.send(albums)
-  } catch (error) {
-    console.error(`Error listing albums: ${error}`)
-    resp.status(500).send('Error listing albums')
   }
 }
 
@@ -33,14 +25,28 @@ export const getAlbum = async (req, resp) => {
     const album = await Album.getById(id)
 
     if (!album) {
-      resp.sendStatus(404)
+      // resp.sendStatus(404)
+      resp.status(404).json({ message: 'Album not found' })
     } else {
-      resp.json(album)
+      // resp.json(album)
+      resp.status(200).json({ status: 'success', data: album })
     }
   } catch (error) {
     console.error(`Error getting album: ${error}`)
 
     resp.status(500).json({ message: error })
+  }
+}
+
+export const listAlbums = async (req, resp) => {
+  console.log(req.body)
+  try {
+    const albums = await Album.listAll()
+    // resp.send(albums)
+    resp.status(200).json({ status: 'success', data: albums })
+  } catch (error) {
+    console.error(`Error listing albums: ${error}`)
+    resp.status(500).send('Error listing albums')
   }
 }
 
@@ -50,11 +56,11 @@ export const updateAlbum = async (req, resp) => {
     const albumToUpdate = req.body
     const albumUpdated = await Album.update(id, albumToUpdate)
 
-    resp.json(albumUpdated)
+    resp.status(200).json({ message: 'Album updated', data: albumUpdated })
   } catch (error) {
     console.error(`Error updating album: ${error}`)
 
-    resp.sendStatus(500).json({ message: 'Error updating album' })
+    resp.status(500).json({ message: 'Error updating album' })
   }
 }
 
@@ -62,15 +68,17 @@ export const deleteAlbum = async (req, resp) => {
   try {
     const { id } = req.params
     const isDeleted = await Album.delete(id)
+    console.log(isDeleted instanceof Error)
 
     if (!isDeleted) {
-      resp.sendStatus(404)
+      // resp.sendStatus(404)
+      resp.status(404).json({ message: 'Album not found' })
+    } else {
+      resp.status(204).json({ message: 'Album deleted' })
     }
-
-    resp.sendStatus(204)
   } catch (error) {
     console.error(`Error deleting album: ${error}`)
 
-    resp.sendStatus(500).json({ message: 'Error deleting album' })
+    resp.status(500).json({ message: 'Error deleting album' })
   }
 }
