01a13fb258b6891d5bef6995d7841219239e55f7
Nelson Rivas <nelson.rivas.ucv+code@gmail.com>
2023-02-25 16:39:54 -0400

    Cleaning dev environment

--- src/config/environment/dev.js
+++ src/config/environment/dev.js
@@ -3,15 +3,11 @@ dotenv.config()
 
 const DEV_CONFIG = {
   PORT: process.env.PORT,
-  BASE_URL: 'http://localhost:3000',
   DB_HOST: process.env.DB_HOST,
   DB_NAME: process.env.DB_NAME,
   DB_PORT: process.env.DB_PORT,
-  DB_USER: process.env.DB_USERNAME,
+  DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD
 }
 
-const { BASE_URL } = DEV_CONFIG
-const myUrl = `${BASE_URL}/api/v1`
-
-export { DEV_CONFIG, myUrl }
+export { DEV_CONFIG }
